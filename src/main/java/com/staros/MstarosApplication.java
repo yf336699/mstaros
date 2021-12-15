0cc869b76455738dca7363e43f0777715dcdb7a9
12802 <12802>
2021-12-15 14:46:16 +0800

    构建

---
+++ src/main/java/com/staros/MstarosApplication.java
@@ -0,0 +1,13 @@
+package com.staros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstarosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MstarosApplication.class, args);
+    }
+
+}
